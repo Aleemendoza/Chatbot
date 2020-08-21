c1f62914ca27b6a8f6a5e85dd5245ebbd0c66b86
Aleemendoza <alejandromendoza.dev@gmail.com>
2020-08-21 17:46:28 -0300

    dockerfile de front end, back end, and dockercompose

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,23 +1,17 @@
 const express = require('express');
 const app = express();
-const passport = require('passport');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-const env = require('dotenv').load();
+const env = require('dotenv')
 
-app.use(bodyParser.urlencoded({extended = true}));
+app.use(bodyParser.urlencoded({extended:  true}));
 app.use(bodyParser.json());
-
-app.use(session({secret : 'keyboard cat', resave: true, saveUninitialized: true}));
-app.use(passport());
-app.use(session());
 
 app.get('/', (req, res) => {
     res.send('bienvenido usuarios!');
 
 });
 
-app.listen(3000, (err) => {
+app.listen(3001, (err) => {
     if (!err) {
         console.log('funciona')
     } else
